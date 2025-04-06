2a7e189046262125b975328efe9d53862c21b938
arjunsk <arjunsk@users.noreply.github.com>
2025-04-05 20:02:36 -0700

    init

--- main.cpp
+++ main.cpp
@@ -9,17 +9,14 @@ int main() {
     TABLE table;
     table.file = new ha_innobase(nullptr, nullptr);
 
-    // Scan rows using our iterator
     ha_rows examined_rows = 0;
     TableScanIterator iter(nullptr, &table, 3.0, &examined_rows);
 
-    if (!iter.Init()) {
-        int res;
-        while (!(res = iter.Read())) {
-            std::cout << "Row: " << iter.m_record << std::endl;
-        }
-    } else {
-        std::cerr << "Failed to initialize table scan.\n";
+    if (iter.Init())
+        return true;
+
+    while (iter.Read() == 0) {
+        std::cout << "Row: " << iter.m_record << std::endl;
     }
 
     return 0;
--- sql/iterators/row_iterator.h
+++ sql/iterators/row_iterator.h
@@ -18,23 +18,6 @@ public:
 
     virtual int Read() = 0;
 
-
-    // In certain queries, such as SELECT FOR UPDATE, UPDATE or DELETE queries,
-    // reading rows will automatically take locks on them. (This means that the
-    // set of locks taken will depend on whether e.g. the optimizer chose a table
-    // scan or used an index, due to InnoDB's row locking scheme with “gap locks”
-    // for B-trees instead of full predicate locks.)
-    //
-    // However, under some transaction isolation levels (READ COMMITTED or
-    // less strict), it is possible to release such locks if and only if the row
-    // failed a WHERE predicate, as only the returned rows are protected,
-    // not _which_ rows are returned. Thus, if Read() returned a row that you did
-    // not actually use, you should call UnlockRow() afterwards, which allows the
-    // storage engine to release the row lock in such situations.
-    //
-    // TableRowIterator has a default implementation of this; other iterators
-    // should usually either forward the call to their source iterator (if any)
-    // or just ignore it. The right behavior depends on the iterator.
     virtual void UnlockRow() = 0;
 
 protected:
